08b0d837d594dd7345a1e47aeab9df4e46ab6b1c
linnearothin <linnea.rothin@hotmail.com>
2017-09-09 19:41:40 +0200

    Added js lib

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+function readIn(){
+	$('#calender').fullCalendar({
+		    defaultView: 'timelineMonth',
+		    events: [
+		        dinner, party
+		    ],
+		    resources: [
+		        testing, other, cat,
+		    ]
+		    // other options go here...
+		});
+
+};
+
+$('#tcalender').fullCalendar({
+	schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives'
+});
+
+});
